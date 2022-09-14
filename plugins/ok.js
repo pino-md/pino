295f62a636298a5a04f5b58889b67fe45b151801
pino-md <bisnispinomd@gmail.com>
2022-09-14 17:21:13 +0700

    Update ok.js

--- plugins/ok.js
+++ plugins/ok.js
@@ -3,11 +3,11 @@ const { sticker } = require('../lib/sticker')
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async(m, { conn }) => {
-  let stiker = await sticker(null, 'https://upload.kilers-beta.eu.org/puApulsV5M6D.png', 'BOT ONLINE', 'Zahra Wibu')
+  let stiker = await sticker(null, 'https://telegra.ph/file/48554501942147dc354b9.jpg', 'WM', 'ZAHRA')
   if (stiker) return conn.sendFile(m.chat, stiker, 'ONLINE.webp', '', m, false, { asSticker: true })
   throw stiker.toString()
 }
-handler.customPrefix = /^(tes|bot|bot online?|oy bot|Trimakasi|tq|thanks|trimakasih|makasi|hai bot|)$/i
+handler.customPrefix = /^(tes|bot|bot online?|oy bot|trimakasi|tq|thanks|ok|oke|mksi|trimakasih|makasi|hai bot|)$/i
 handler.command = new RegExp
 
 module.exports = handler
