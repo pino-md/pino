61df1123b78d39ff3642b601c9117a17fc79916c
pino-md <bisnispinomd@gmail.com>
2022-09-15 18:31:47 +0700

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -1,5 +1,5 @@
 let handler = async(m,{text, conn}) => {
-let supa = 'https://restapi-pino.herokuapp.com/api/wallpaper/nsfwloli?apikey=kontol'
+let supa = 'https://docs-pinomodz.herokuapp.com/api/wallpaper/nsfwloli?apikey=kontol'
 conn.sendFile(m.chat, supa, null, 'Nih', m)
 }
 handler.help = ['loli']
